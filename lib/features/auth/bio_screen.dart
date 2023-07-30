9954019a5207eb4976f4cfe6ca11d52c61741b0e
Ahmed <ahmedzon0598596843@gmail.com>
2023-07-30 21:00:11 +0300

    update - font

--- lib/features/auth/bio_screen.dart
+++ lib/features/auth/bio_screen.dart
@@ -1,11 +1,22 @@
 
 
+
+
+
+
+
+
+
+import 'dart:convert';
+
 import 'package:application/core/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../../core/resources/manager_colors.dart';
 import 'package:jiffy/jiffy.dart';
+import 'package:http/http.dart'as http;
+import 'package:image_picker/image_picker.dart';
 
 class bio extends StatefulWidget {
   const bio({super.key});
@@ -17,14 +28,12 @@ class bio extends StatefulWidget {
 }
 
 class _bioState extends State<bio> {
-   DateTime a1 = DateTime.now().subtract(Duration(days: 365));
-   cheng() async {
-     await Jiffy.setLocale('ar');
-   }
+
+
   
   @override
   void initState() {
-    cheng();
+
     super.initState();
 
   }
@@ -45,12 +54,8 @@ class _bioState extends State<bio> {
       extendBodyBehindAppBar: true,
       body: Column(
         children: [
-          Container(height: 400,width: double.infinity,color: Colors.black,),
-          TextButton(onPressed: (){
-           print(Jiffy.parse(a1.toString()).fromNow());
-          }, child: Text("add"),style: TextButton.styleFrom(backgroundColor: Colors.black),),
-          Text("sadfasdas",style: TextStyle(fontFamily: "ReemKufiFun",fontSize: 50),),
-          Text("sadfasdas",style: TextStyle(fontFamily: "",fontSize: 50),),
+
+
         ],
       )
     );
